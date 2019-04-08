301486fdcb13261059d46e1a9bb41d7083585706
Nick Lee <leexnick@gmail.com>
2019-04-08 09:47:28 -0700

    add Stack class

--- classes.js
+++ classes.js
@@ -1,29 +1,49 @@
-'use strict'
+/* eslint-disable indent */
+'use strict';
 
 class Card {
-
-    constructor(suit, value) {
-        this.suit = suit;
-        this.value = value;
-        this.face = false;
-    };
-
+  constructor(suit, value) {
+    this.suit = suit;
+    this.value = value;
+    this.face = false;
+  }
 }
-const drawStack = [];
+
+class Stack {
+    constructor(stackArr) {
+        this.stack = [...stackArr];
+    }
+
+    drawCard() {
+      return this.stack.pop();
+    }
+
+    addCard() {
+
+    }
+}
+
+
+const drawStackArr = [];
+
+
+ ///new Stack(drawStack).drawCard = card.. GAME: activeStack.drawCard() = playedCard -> discardStack -> drawStack.drawCard -> activeStack.push(card)
 const activeStack = [];
 const discardStack = [];
+
 const SUITS = ['spades', 'hearts', 'diamonds', 'clubs'];
 const VALUES = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
 (function generateDeck() {
-    SUITS.forEach(suit => {
-        VALUES.forEach(value => {
-            drawDeck.push(new Card(suit, value));
-        })
-    })
+  SUITS.forEach(suit => {
+    VALUES.forEach(value => {
+      drawStackArr.push(new Card(suit, value));
+    });
+  });
 } () );
 
+const drawStack = new Stack(drawStackArr);
+const draw = drawStack.drawCard();
 
-
-
-console.log(deck);
+console.log(drawStackArr);
+console.log(draw);
